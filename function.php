e2382b34e66795212e3bfb4f8a6c42f5705c563d
zhouyuan1 <zhouyuan1@staff.weibo.com>
2017-01-03 16:43:31 +0800

    change log location

--- function.php
+++ function.php
@@ -5,7 +5,7 @@ function log_writer($operation_name, $operation_info)
     $tt = date("Y-M-D-d-H-i-s");
     $ip = $_SERVER["REMOTE_ADDR"];
 
-    $fw = fopen("operation.log", "a+");
+    $fw = fopen("../operation.log", "a+");
     fwrite($fw, "[" . $ip . "] [" . $tt . "] " . $operation_name . " " . $operation_info . "\n");
     fclose($fw);
 }
@@ -16,7 +16,7 @@ function http_log_writer($operation_name, $operation_info)
     $tt = date("Y-M-D-d-H-i-s");
     $ip = $_SERVER["REMOTE_ADDR"];
 
-    $fw = fopen("operation.log", "a+");
+    $fw = fopen("../operation.log", "a+");
     fwrite($fw, "[" . $ip . "] [" . $tt . "] " . $operation_name . " " . $operation_info . "\n");
     fclose($fw);
 }
